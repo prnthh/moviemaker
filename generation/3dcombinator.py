a408127e76a8fbfbeaa25954943c793fe6c0d2d8
prnth <pranith_h@yahoo.com.sg>
2023-11-18 07:39:28 +0530

    combinator fixed

--- generation/3dcombinator.py
+++ generation/3dcombinator.py
@@ -10,8 +10,6 @@ model_path = os.path.join(current_dir, 'model.fbx')
 print(model_path)
 bpy.ops.import_scene.fbx(filepath=model_path)
 
-json = {'hat': 'brown_cowboy', 'glasses': 'round2', 'hair_style': 'long', 'necklace': 'pearl necklace', 'shirt': 'Maf Creeper', 'race': 'clay', 'hair_color': 'dark', 'face': 'teardrops tattoo', 'eye_color': 'brown', 'hidden': 'nose blush'}
-
 def match_transformations(obj, target_obj):
     print(obj)
     rotation_x = 180  # Rotate 45 degrees around X-axis
@@ -38,53 +36,55 @@ def attach_to_head(obj_name, head_bone):
     match_transformations(obj, armature)
 
 # Replace the image texture of the 'Skin' material
-def replace_skin_texture(new_image_path):
+def replace_skin_texture(modelId):
+    new_image_path = os.path.join(current_dir, f'output/{modelId}.png')
+    new_image = bpy.data.images.load(new_image_path)
     # Iterate over all materials
     for mat in bpy.data.materials:
-        if mat.name == 'Skin' and mat.node_tree:
+        if 'Skin' in mat.name and mat.node_tree:
             for node in mat.node_tree.nodes:
                 # Find the image texture node
                 if node.type == 'TEX_IMAGE':
+                    if node.image:
+                        bpy.data.images.remove(node.image, do_unlink=True)
                     # Replace the image
-                    node.image = bpy.data.images.load(new_image_path)
-                    break
+                    node.image = new_image
 
-if json['hair_style'] is not None:
-    glb_file_path = f"{current_dir}props/hair_{json['hair_style'].replace(' ', '_')}.glb"
-    bpy.ops.import_scene.gltf(filepath=glb_file_path)
-    attach_to_head("Hairmodel", 'mixamorig:Head')
+def replace_from_json(json):
+    if json['hair_style'] is not None:
+        glb_file_path = f"{current_dir}props/hair_{json['hair_style'].replace(' ', '_')}.glb"
+        bpy.ops.import_scene.gltf(filepath=glb_file_path)
+        attach_to_head("Hairmodel", 'mixamorig:Head')
 
-if json['glasses'] is not None:
-    glb_file_path = f"{current_dir}props/glasses_{json['glasses'].replace(' ', '_')}.glb"
-    bpy.ops.import_scene.gltf(filepath=glb_file_path)
-    attach_to_head("Sketchfab_model", 'mixamorig:Head')
+    if json['glasses'] is not None:
+        glb_file_path = f"{current_dir}props/glasses_{json['glasses'].replace(' ', '_')}.glb"
+        bpy.ops.import_scene.gltf(filepath=glb_file_path)
+        attach_to_head("Sketchfab_model", 'mixamorig:Head')
+        
+    if json['hat'] is not None:
+        glb_file_path = f"{current_dir}props/hat_{json['hat'].replace(' ', '_')}.glb"
+        bpy.ops.import_scene.gltf(filepath=glb_file_path)
+        attach_to_head("Hatmodel", 'mixamorig:Head')
     
-if json['hat'] is not None:
-    glb_file_path = f"{current_dir}props/hat_{json['hat'].replace(' ', '_')}.glb"
-    bpy.ops.import_scene.gltf(filepath=glb_file_path)
-    attach_to_head("Hatmodel", 'mixamorig:Head')
+def write_model(modelId):
+    for texture in bpy.data.textures:
+        # Check if the texture has no users
+        if texture.users == 0:
+            # Remove the texture
+            bpy.data.textures.remove(texture)
+    # Export the combined model
+    output_model_path = os.path.join(current_dir, f'output/{current_model}.fbx')
+    bpy.ops.export_scene.fbx(
+        filepath=output_model_path,
+        path_mode='COPY',  # Copy all external files into the destination directory
+        embed_textures=True,  # Embed textures in the FBX binary file
+        bake_space_transform=False 
+    ) 
 
-#if helmet:
-    # Select the helmet
-#    bpy.context.view_layer.objects.active = helmet
-#    helmet.select_set(True)
 
-    # Apply rotation and scale transformations
-#    bpy.ops.object.transform_apply(location=True, rotation=True, scale=True)
+json = {'hat': 'trucker im sooo', 'glasses': 'round2', 'hair_style': 'long', 'necklace': 'pearl necklace', 'shirt': 'Maf Creeper', 'race': 'clay', 'hair_color': 'dark', 'face': 'teardrops tattoo', 'eye_color': 'brown', 'hidden': 'nose blush'}
+current_model = 1
+replace_from_json(json)
+replace_skin_texture(current_model)
+write_model(current_model)
 
-    # Attach the helmet to the head bone
-#    attach_to_head(helmet.name, 'mixamorig:Head')  # Replace 'HeadBoneName' with your bone name
-
-
-# Call the function with the path to the new image
-new_image_path = os.path.join(current_dir, 'output/1.png')
-replace_skin_texture(new_image_path)
-
-# Export the combined model
-output_model_path = os.path.join(current_dir, 'output_model.fbx')
-bpy.ops.export_scene.fbx(
-    filepath=output_model_path,
-    path_mode='COPY',  # Copy all external files into the destination directory
-    embed_textures=True,  # Embed textures in the FBX binary file
-    bake_space_transform=False 
-)
