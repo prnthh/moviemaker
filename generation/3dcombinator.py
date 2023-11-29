209db09436fa19c42a1d62edff0411f997fcceb9
prnth <pranith_h@yahoo.com.sg>
2023-11-29 21:11:39 +0530

    vrm base, glb base, new exporter

--- generation/3dcombinator.py
+++ generation/3dcombinator.py
@@ -6,7 +6,7 @@ import json
 current_dir = "/Users/prnth/Documents/GitHub/moviemaker/generation/"
 print("Current Directory:", current_dir)
 
-def match_transformations(obj, target_obj):
+def match_transformations(obj, target_obj, vrm=False):
     print(obj)
     rotation_x = 180  # Rotate 45 degrees around X-axis
     # Convert degrees to radians
@@ -15,21 +15,25 @@ def match_transformations(obj, target_obj):
     # Apply the rotations to the object
     obj.rotation_euler = (rotation_x_rad, 0, 0)
     # Match the location, rotation, and scale of the target object
-    obj.location = (0, 6, 21)
-    obj.scale = (10, 10, 10)   # target_obj.scale
+    if not vrm:
+        obj.location = (0, 6, 21)
+        obj.scale = (10, 10, 10)   # target_obj.scale
+    else:
+        obj.location = (0, 0.08, 0.2)
+        obj.scale = (0.1, 0.1, 0.1)
     
 # Function to attach a helmet/hair to the head bone
-def attach_to_head(obj_name, head_bone):
+def attach_to_head(obj_name, head_bone, vrm=False):
     obj = bpy.data.objects[obj_name]
     bpy.context.view_layer.objects.active = obj
     obj.select_set(True)
     # Apply rotation and scale transformations
-    bpy.ops.object.transform_apply(location=True, rotation=False, scale=False)
+    bpy.ops.object.transform_apply(location=True, rotation=False, scale=True)
     armature = bpy.data.objects['Armature']  # Replace with your armature's name
     obj.parent = armature
     obj.parent_type = 'BONE'
     obj.parent_bone = head_bone
-    match_transformations(obj, armature)
+    match_transformations(obj, armature, vrm)
 
 # Replace the image texture of the 'Skin' material
 def replace_skin_texture(modelId, json):
@@ -67,24 +71,24 @@ def replace_skin_texture(modelId, json):
                         # Replace the image
                     node.image = mouth_image
 
-def replace_from_json(json):
+def replace_from_json(json, vrm=False):
     if json['hair_style'] is not None and json['hair_style'] != 'bald':
         print(f"adding {json['hair_style']}")
         glb_file_path = f"{current_dir}props/hair_{json['hair_style'].replace(' ', '_')}.glb"
         bpy.ops.import_scene.gltf(filepath=glb_file_path)
-        attach_to_head("Hairmodel", 'mixamorig:Head')
+        attach_to_head("Hairmodel", 'mixamorig:Head', vrm)
 
     if json['glasses'] is not None and json['glasses'] != 'none':
         print(f"adding {json['glasses']}")
         glb_file_path = f"{current_dir}props/glasses_{json['glasses'].replace(' ', '_')}.glb"
         bpy.ops.import_scene.gltf(filepath=glb_file_path)
-        attach_to_head("Sketchfab_model", 'mixamorig:Head')
+        attach_to_head("Sketchfab_model", 'mixamorig:Head', vrm)
         
     if json['hat'] is not None and json['hat'] != 'none':
         print(f"adding {json['hat']}")
         glb_file_path = f"{current_dir}props/hat_{json['hat'].replace(' ', '_')}.glb"
         bpy.ops.import_scene.gltf(filepath=glb_file_path)
-        attach_to_head("Hatmodel", 'mixamorig:Head')
+        attach_to_head("Hatmodel", 'mixamorig:Head', vrm)
     
 def write_model(modelId):
     for texture in bpy.data.textures:
@@ -94,21 +98,23 @@ def write_model(modelId):
             bpy.data.textures.remove(texture)
     # Export the combined model
     output_model_path = os.path.join(current_dir, f'output/{current_model}.glb')
-#    bpy.ops.export_scene.fbx(
-#        filepath=output_model_path,
-#        path_mode='COPY',  # Copy all external files into the destination directory
-#        embed_textures=True,  # Embed textures in the FBX binary file
-#        bake_space_transform=False 
-#    )
     export_settings = {
         'use_selection': False,  # Set to True if you want to export only selected objects
         'export_format': 'GLB',  # Specify GLB format
         'export_apply': True,    # Apply modifiers and other settings
         'filepath': output_model_path
     }
-
-    # Export the scene to GLB
     bpy.ops.export_scene.gltf(**export_settings)
+    
+def write_model_vrm(modelId):
+    for texture in bpy.data.textures:
+        # Check if the texture has no users
+        if texture.users == 0:
+            # Remove the texture
+            bpy.data.textures.remove(texture)
+    # Export the combined model
+    output_model_path = os.path.join(current_dir, f'upload/{current_model}.vrm')
+    bpy.ops.export_scene.vrm('EXEC_DEFAULT', filepath=output_model_path)
     
 def clear_workspace():
     obs = [o for o in bpy.data.objects
@@ -137,16 +143,22 @@ startrange = 1
 intervalsize = 1
 
 for current_model in range(startrange, startrange + intervalsize):
-    clear_workspace()
-    clear_workspace()
-    # Load the base model
-    model_path = os.path.join(current_dir, 'model.glb')
-#    bpy.ops.import_scene.fbx(filepath=model_path)
-    bpy.ops.import_scene.gltf(filepath=model_path)
     file_path = os.path.join(current_dir, f'output/{current_model}.json')
     with open(file_path, 'r') as file:
         data = json.load(file)
+        
+    # Load the base model
+    clear_workspace()
+    model_path = os.path.join(current_dir, 'model.glb')
+    bpy.ops.import_scene.gltf(filepath=model_path)
     replace_from_json(data)
     replace_skin_texture(current_model, data)
     write_model(current_model)
+    
+    clear_workspace()
+    model_path = os.path.join(current_dir, 'model.vrm')
+    bpy.ops.import_scene.vrm(filepath=model_path)
+    replace_from_json(data, True)
+    replace_skin_texture(current_model, data)
+    write_model_vrm(current_model)
 
--- generation/gifgen.sh
+++ generation/gifgen.sh
@@ -6,7 +6,7 @@ endrange=$(($startrange + $intervalsize))
 
 for (( i=$startrange; i<$endrange; i+=1 ))
 do
-    ffmpeg -y -i output/$i.mkv -vf "fps=10,scale=400:-1:flags=lanczos" -c:v gif -f gif output/$i.gif
+    ffmpeg -y -i output/$i.mkv -vf "fps=10,scale=480:-1:flags=lanczos" -c:v gif -f gif output/$i.gif
     gifsicle -O3 output/$i.gif -o upload/$i.gif
 done
 
